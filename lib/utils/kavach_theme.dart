b532d1fd0930bd446b9099e6f5e12ab0dd3ce2e2
7-USH <tush1245@gmail.com>
2023-04-12 11:50:28 +0530

    Initial Push | new App added

---
+++ lib/utils/kavach_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class KavachTheme {
+  static const Color lightPink = Color(0xffF06CAB);
+  static const Color darkPink = Color(0xffEA4E97);
+  static const Color redishPink = Color(0xff530F40);
+  static const Color nearlyGrey = Color(0xff5A616B);
+  static const Color lightGrey = Color(0xffB8B8B8);
+  static const Color pureWhite = Color.fromARGB(255, 255, 255, 255);
+  static const Color darkishGrey = Color(0xff161616);
+}
