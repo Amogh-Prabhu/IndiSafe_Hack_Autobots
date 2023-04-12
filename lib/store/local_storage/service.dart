14d45f432cdeee4588f17da3837a87eb166bfbc4
Soumitra Kand <soumitrakand3@gmail.com>
2023-04-13 02:24:26 +0530

    Settings Page

--- lib/store/local_storage/service.dart
+++ lib/store/local_storage/service.dart
@@ -4,19 +4,19 @@ import '../models/settings_model.dart';
 class SharedPreferenceService {
   static void putData(SettingsModel model) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('uid', model.uid);
-    await prefs.setStringList('contact_numbers', model.contactNumbers);
-    await prefs.setStringList('triggers', model.triggers);
-    await prefs.setStringList('actions', model.actions);
+    await prefs.setString('uid', model.uid!);
+    await prefs.setStringList('contact_numbers', model.contactNumbers!);
+    await prefs.setStringList('triggers', model.triggers!);
+    await prefs.setStringList('actions', model.actions!);
   }
 
   static Future<SettingsModel> getData() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    final String uid = prefs.getString("uid")!;
-    final List<String> contactNumbers = prefs.getStringList('contact_numbers')!;
-    final List<String> triggers = prefs.getStringList('triggers')!;
-    final List<String> actions = prefs.getStringList('actions')!;
+    final String? uid = prefs.getString("uid");
+    final List<String>? contactNumbers = prefs.getStringList('contact_numbers');
+    final List<String>? triggers = prefs.getStringList('triggers');
+    final List<String>? actions = prefs.getStringList('actions');
 
     return SettingsModel(
       uid: uid,
