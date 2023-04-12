313fd6d2bd95f75cba61e66c623e3215a8bb2173
Sayali <sayalipanch17@gmail.com>
2023-04-12 17:02:46 +0530

    initial commit

---
+++ lib/settings/settings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
