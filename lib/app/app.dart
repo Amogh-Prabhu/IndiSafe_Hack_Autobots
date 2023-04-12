d64c63ebe6cbbf982d3c42721edc763234815581
7-USH <tush1245@gmail.com>
2023-04-12 16:53:23 +0530

    Added NavBar

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, library_private_types_in_public_api
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kavach/track/track.dart';
 import 'package:kavach/utils/kavach_theme.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
@@ -33,40 +34,70 @@ class _AppState extends State<App> {
       onWillPop: () async => false,
       child: PersistentTabView(
         context,
+        navBarHeight: size / 6,
+        padding: NavBarPadding.only(top: 20),
         backgroundColor: KavachTheme.pureWhite,
         screenTransitionAnimation: const ScreenTransitionAnimation(
           animateTabTransition: true,
           curve: Curves.easeIn,
         ),
         decoration: NavBarDecoration(
-            border:
-                Border(top: BorderSide(color: Colors.grey.withOpacity(0.2)))),
-        screens: [Scaffold(), Scaffold(),Scaffold()],
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.08),
+                offset: const Offset(0.1, 0.8),
+                spreadRadius: 3,
+                blurRadius: 3)
+          ],
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(
+                30,
+              ),
+              topRight: Radius.circular(30)),
+        ),
+        screens: [Track(), Scaffold(), Scaffold(), Scaffold(), Scaffold()],
         items: [
           PersistentBottomNavBarItem(
-              icon: Icon(CupertinoIcons.house, size: size / 17),
+              icon: Icon(CupertinoIcons.location_circle, size: size / 17),
               inactiveColorPrimary: Colors.grey,
-              activeColorSecondary: Colors.white,
+              activeColorSecondary: KavachTheme.lightPink,
               activeColorPrimary: KavachTheme.redishPink,
-              title: "Home",
+              title: "Track Me",
               textStyle: KavachTheme.subtitleText(
-                  size: size / 30, weight: FontWeight.w600)),
+                  size: size / 35, weight: FontWeight.w600)),
           PersistentBottomNavBarItem(
-              icon: Icon(CupertinoIcons.house, size: size / 17),
+              icon: Icon(CupertinoIcons.recordingtape, size: size / 17),
               inactiveColorPrimary: Colors.grey,
-              activeColorSecondary: Colors.white,
+              activeColorSecondary: KavachTheme.lightPink,
               activeColorPrimary: KavachTheme.redishPink,
-              title: "Home",
+              title: "Record",
               textStyle: KavachTheme.subtitleText(
-                  size: size / 30, weight: FontWeight.w600)),
+                  size: size / 35, weight: FontWeight.w600)),
           PersistentBottomNavBarItem(
-              icon: Icon(CupertinoIcons.house, size: size / 17),
+              icon: Icon(Icons.sos_sharp, size: size / 13),
               inactiveColorPrimary: Colors.grey,
               activeColorSecondary: Colors.white,
-              activeColorPrimary: KavachTheme.redishPink,
-              title: "Home",
+              activeColorPrimary: Colors.redAccent,
               textStyle: KavachTheme.subtitleText(
-                  size: size / 30, weight: FontWeight.w600)),
+                  size: size / 35,
+                  weight: FontWeight.bold,
+                  color: KavachTheme.lightGrey)),
+          PersistentBottomNavBarItem(
+              icon: Icon(CupertinoIcons.book_fill, size: size / 17),
+              inactiveColorPrimary: Colors.grey,
+              activeColorSecondary: KavachTheme.lightPink,
+              activeColorPrimary: KavachTheme.redishPink,
+              title: "Helpline",
+              textStyle: KavachTheme.subtitleText(
+                  size: size / 35, weight: FontWeight.w600)),
+          PersistentBottomNavBarItem(
+              icon: Icon(CupertinoIcons.settings, size: size / 17),
+              inactiveColorPrimary: Colors.grey,
+              activeColorSecondary: KavachTheme.lightPink,
+              activeColorPrimary: KavachTheme.redishPink,
+              title: "Settings",
+              textStyle: KavachTheme.subtitleText(
+                  size: size / 35, weight: FontWeight.w600)),
         ],
         controller: _controller,
         confineInSafeArea: true,
@@ -76,7 +107,7 @@ class _AppState extends State<App> {
         hideNavigationBarWhenKeyboardShows: false,
         popAllScreensOnTapOfSelectedTab: true,
         popActionScreens: PopActionScreensType.all,
-        navBarStyle: NavBarStyle.style10,
+        navBarStyle: NavBarStyle.style15,
       ),
     );
   }
--- lib/login/login.dart
+++ lib/login/login.dart
@@ -16,80 +16,87 @@ class _LoginState extends State<Login> {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: KavachTheme.pureWhite,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Kavach",
-                    style: KavachTheme.titleText(
-                        size: width / 12,
-                        isShadow: true,
-                        weight: FontWeight.w900,
-                        color: KavachTheme.darkPink),
-                  ),
-                  Text(
-                    "Personal app",
-                    style: KavachTheme.subtitleText(
-                        size: width / 20,
-                        weight: FontWeight.bold,
-                        isShadow: true),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: width / 5,
-            ),
-            Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: width / 1.2,
+            width: width,
+            decoration: BoxDecoration(
+                color: KavachTheme.redishPink,
+      
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(width / 3.5),
+                    bottomLeft: Radius.circular(width / 3.5))),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  "Sign Up/Log in with email address",
+                  "Kavach",
+                  style: KavachTheme.titleText(
+                      size: width / 9,
+                      isShadow: true,
+                      weight: FontWeight.w900,
+                      color: KavachTheme.darkPink),
+                ),
+                Text(
+                  "Women Safety App",
                   style: KavachTheme.subtitleText(
-                      size: width / 25,
+                      size: width / 23,
                       weight: FontWeight.bold,
-                      color: KavachTheme.darkishGrey.withOpacity(0.7)),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: width / 2.5,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      PersistentNavBarNavigator.pushNewScreen(context,
-                          screen: const LoginScreen(),
-                          withNavBar: false,
-                          pageTransitionAnimation:
-                              PageTransitionAnimation.scale);
-                    },
-                    style: KavachTheme.buttonStyle(
-                      backColor: KavachTheme.redishPink,
-                    ),
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Text(
-                          "Let's Go!",
-                          style: KavachTheme.subtitleText(
-                              size: width / 25,
-                              weight: FontWeight.bold,
-                              color: KavachTheme.pureWhite),
-                        ),
+                      isShadow: true),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: width / 6,
+          ),
+          Column(
+            children: [
+              Text(
+                "Sign Up/Log in with email address",
+                style: KavachTheme.subtitleText(
+                    size: width / 27,
+                    weight: FontWeight.bold,
+                    color: KavachTheme.darkishGrey.withOpacity(0.7)),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: width / 2.5,
+                child: ElevatedButton(
+                  onPressed: () {
+                    PersistentNavBarNavigator.pushNewScreen(context,
+                        screen: const LoginScreen(),
+                        withNavBar: false,
+                        pageTransitionAnimation: PageTransitionAnimation.scale);
+                  },
+                  style: KavachTheme.buttonStyle(
+                    backColor: KavachTheme.redishPink,
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        "Let's Go!",
+                        style: KavachTheme.subtitleText(
+                            size: width / 25,
+                            weight: FontWeight.bold,
+                            color: KavachTheme.pureWhite),
                       ),
                     ),
                   ),
-                )
-              ],
-            )
-          ],
-        ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
